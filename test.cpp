5f5f046c8dff9ca26fa07c69dbdb11bfeed80714
Saturnalia7 <comiller76@hotmail.com>
2022-02-07 20:15:53 -0600

    cin addition

--- test.cpp
+++ test.cpp
@@ -6,7 +6,9 @@ int main(int argc, char **argv) {
 	std::cout << "Test print\n";
 	std::cout << argc << "\n";
 	std::cout << argv[0] << "\n";
-	std::cout << i;
+	std::cout << i << "\n";
+	std::cin >> i; // put input into i
+	std::cout << i; // print i
 	return 0;
 }
 
