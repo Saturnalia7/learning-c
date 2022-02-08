b6a7e0f85dfc094be034cdbf8b5745c3d9820748
Saturnalia7 <comiller76@hotmail.com>
2022-02-07 20:08:16 -0600

    variables and sys args

--- test.cpp
+++ test.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
-using namespace std;
 
-int main(int argc, char **argv){
-	cout << "Test print";
+int i{ 5 };
+
+int main(int argc, char **argv) {
+	std::cout << "Test print\n";
+	std::cout << argc << std::endl;
+	std::cout << argv[0] << std::endl;
+	std::cout << i;
 	return 0;
 }
+
